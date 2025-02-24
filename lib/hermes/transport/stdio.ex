955f8b69422e53a9e3e2189fa5a25875863cfc33
Zoey Pessanha <zoey.spessanha@cloudwalk.io>
2025-02-24 19:38:05 -0300

    wip

--- lib/hermes/transport/stdio.ex
+++ lib/hermes/transport/stdio.ex
@@ -9,9 +9,9 @@ defmodule Hermes.Transport.STDIO do
 
   @behaviour Transport
 
-  @type t :: %__MODULE__{port: Port.t(), params: params_t}
+  @type t :: %__MODULE__{port: port(), params: params_t, ref: reference()}
 
-  defstruct [:port, :params]
+  defstruct [:port, :params, :ref]
 
   @type params_t :: Enumerable.t(option)
   @type option ::
@@ -48,13 +48,22 @@ defmodule Hermes.Transport.STDIO do
   end
 
   @impl Transport
-  def send(pid, _) do
-    IO.puts("Sending message to #{inspect(pid)}")
+  def send(pid, %{} = message) do
+    GenServer.call(pid, {:send, message})
   end
 
   @impl Transport
   def subscribe(pid) do
     IO.puts("Subscribed to #{inspect(pid)}")
+  end
+
+  @impl Transport
+  def notify(pid, notification) do
+    GenServer.call(pid, {:notify, notification})
+  end
+
+  def shutdown(pid) do
+    GenServer.cast(pid, :close_port)
   end
 
   @impl GenServer
@@ -71,18 +80,10 @@ defmodule Hermes.Transport.STDIO do
   def handle_continue(:spawn, %{params: params} = state) do
     if cmd = System.find_executable(params.command) do
       env = Map.merge(get_default_env(), params.env)
+      port = spawn_port(cmd, state)
+      ref = Port.monitor(port)
 
-      port =
-        Port.open({:spawn_executable, cmd}, [
-          :binary,
-          {:args, params.args},
-          {:env, Map.to_list(env)}
-          # {:cd, params.cwd}
-        ])
-
-      Process.monitor(port)
-
-      {:ok, %{state | port: port, env: env}}
+      {:noreply, %{state | port: port, ref: ref, params: %{params | env: env}}}
     else
       {:stop, {:error, "Command not found: #{params.command}"}, state}
     end
@@ -104,12 +105,35 @@ defmodule Hermes.Transport.STDIO do
     {:stop, status, state}
   end
 
+  def handle_info({:DOWN, ref, :port, port, reason}, %{ref: ref, port: port} = state) do
+    IO.puts("Port monitor DOWN: #{inspect(reason)}")
+    {:stop, reason, state}
+  end
+
   def handle_info({:EXIT, port, reason}, %{port: port} = state) do
     IO.puts("Port exited: #{inspect(reason)}")
     {:stop, reason, state}
   end
 
-  def get_default_env do
+  @impl GenServer
+  def handle_cast(:close_port, %{port: port} = state) do
+    Port.close(port)
+    {:stop, :normal, state}
+  end
+
+  defp spawn_port(cmd, %{params: params}) do
+    env = Map.merge(get_default_env(), params.env) |> normalize_env_for_erlang()
+
+    opts =
+      [:binary]
+      |> then(&if not Enum.empty?(params.args), do: Enum.concat(&1, args: params.args), else: &1)
+      |> then(&if not Enum.empty?(env), do: Enum.concat(&1, env: env), else: &1)
+      |> then(&if params.cwd != "", do: Enum.concat(&1, cd: params.cwd), else: &1)
+
+    Port.open({:spawn_executable, cmd}, opts)
+  end
+
+  defp get_default_env do
     default_env = if :os.type() == {:win32, :nt}, do: @win32_default_env, else: @unix_default_env
 
     System.get_env()
@@ -117,5 +141,11 @@ defmodule Hermes.Transport.STDIO do
     # remove functions, for security risks
     |> Enum.reject(fn {_, v} -> String.starts_with?(v, "()") end)
     |> Enum.into(%{})
+  end
+
+  defp normalize_env_for_erlang(%{} = env) do
+    env
+    |> Map.new(fn {k, v} -> {to_charlist(k), to_charlist(v)} end)
+    |> Enum.into([])
   end
 end
